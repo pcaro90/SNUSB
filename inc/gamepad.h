55b8b414d559b0f5d4a08801a59c55aab7b4a45a
Pablo Caro <me@pcaro.es>
2014-02-18 15:06:10 +0100

    Added gamepad interface

---
+++ inc/gamepad.h
@@ -0,0 +1,83 @@
+// ----------------------------------------------------------------------
+// Copyright (c) 2014 Pablo Caro. All Rights Reserved.
+// Pablo Caro <me@pcaro.es> - https://pcaro.es/
+// gamepad.h
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// ----------------------------------------------------------------------
+
+
+#ifndef __GAMEPAD_H__
+#define __GAMEPAD_H__
+
+#include <util/delay.h>
+
+
+// Button values
+#define BUTTON_A        (0 << 0)
+#define BUTTON_B        (1 << 1)
+#define BUTTON_X        (1 << 2)
+#define BUTTON_Y        (1 << 3)
+#define BUTTON_L        (1 << 4)
+#define BUTTON_R        (1 << 5)
+#define BUTTON_SELECT   (1 << 6)
+#define BUTTON_START    (1 << 7)
+
+// Pad values
+#define JOYPAD_UP       (1 << 0)
+#define JOYPAD_DOWN     (1 << 1)
+#define JOYPAD_LEFT     (1 << 2)
+#define JOYPAD_RIGHT    (1 << 3)
+
+// Virtual values
+#define VIRTUAL_REBOOT   (1 << 0)
+
+
+// Pressed buttons
+#define PRESSED_A         (BUTTON & BUTTON_A)
+#define PRESSED_B         (BUTTON & BUTTON_B)
+#define PRESSED_X         (BUTTON & BUTTON_X)
+#define PRESSED_Y         (BUTTON & BUTTON_Y)
+#define PRESSED_L         (BUTTON & BUTTON_L1)
+#define PRESSED_R         (BUTTON & BUTTON_R1)
+#define PRESSED_START     (BUTTON & BUTTON_START)
+#define PRESSED_SELECT    (BUTTON & BUTTON_SELECT)
+
+// Pressed pad directions
+#define PRESSED_UP        (JOYPAD & JOYPAD_UP)
+#define PRESSED_DOWN      (JOYPAD & JOYPAD_DOWN)
+#define PRESSED_LEFT      (JOYPAD & JOYPAD_LEFT)
+#define PRESSED_RIGHT     (JOYPAD & JOYPAD_RIGHT)
+
+// Pressed virtual buttons
+#define PRESSED_REBOOT    (VIRTUAL & VIRTUAL_REBOOT)
+
+
+// SNES connector interface
+#define CLOCK   (1 << 0)
+#define LATCH   (1 << 1)
+#define DATA    (1 << 2)
+
+
+extern uint8_t BUTTON;
+extern uint8_t JOYPAD;
+extern uint8_t VIRTUAL;
+
+void gamepad_init();
+void gamepad_read();
+void button_read();
+void cycle_latch();
+void cycle_clock();
+
+#endif
---
+++ src/gamepad.c
@@ -0,0 +1,105 @@
+// ----------------------------------------------------------------------
+// Copyright (c) 2014 Pablo Caro. All Rights Reserved.
+// Pablo Caro <me@pcaro.es> - https://pcaro.es/
+// gamepad.c
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// ----------------------------------------------------------------------
+
+uint8_t BUTTON = 0;
+uint8_t JOYPAD = 0;
+uint8_t VIRTUAL = 0;
+
+void gamepad_init()
+{
+    // Configure B port directions
+    DDRB |= CLOCK;  // Use CLOCK bit as output
+    DDRB |= LATCH;  // Use LATCH bit as output
+    DDRB &= ~DATA;  // Use DATA bit as input
+
+    // Configure B port output/input
+    PORTB &= ~CLOCK;  // Set CLOCK bit to 0
+    PORTB &= ~LATCH;  // Set LATCH bit to 0
+    PORTB &= ~DATA;   // Set DATA bit to normal input
+}
+
+void gamepad_read()
+{
+    cycle_latch();
+
+    // Physical buttons
+    BUTTON  = button_read(BUTTON_B);
+    BUTTON |= button_read(BUTTON_Y);
+    BUTTON |= button_read(BUTTON_SELECT);
+    BUTTON |= button_read(BUTTON_START);
+
+    JOYPAD  = joypad_read(JOYPAD_UP);
+    JOYPAD |= joypad_read(JOYPAD_DOWN);
+    JOYPAD |= joypad_read(JOYPAD_LEFT);
+    JOYPAD |= joypad_read(JOYPAD_RIGHT);
+
+    BUTTON |= button_read(BUTTON_A);
+    BUTTON |= button_read(BUTTON_X);
+    BUTTON |= button_read(BUTTON_L);
+    BUTTON |= button_read(BUTTON_R);
+
+    // Virtual buttons
+    // Reboot: Select + Start + L + R
+
+    VIRTUAL = 0;
+
+    if (PRESSED_SELECT) {
+        if (PRESSED_START) {
+            if (PRESSED_L && PRESSED_R) {
+                VIRTUAL = VIRTUAL_REBOOT;
+
+                BUTTON &= ~BUTTON_L;
+                BUTTON &= ~BUTTON_R;
+
+                BUTTON &= ~BUTTON_START;
+                BUTTON &= ~BUTTON_SELECT;
+            }
+        }
+    }
+
+}
+
+void button_read(uint8_t button)
+{
+    return (PINB & DATA) ? 0 : button;
+}
+
+void cycle_latch()
+{
+    // Set LATCH bit to 1
+    PORTB |= LATCH;
+    _delay_us(12);
+
+    // Set LATCH bit to 0
+    PORTB &= ~LATCH;
+    _delay_us(6);
+}
+
+void cycle_clock()
+{
+    // Set CLOCK bit to 1
+    PORTB |= CLOCK;
+    _delay_us(6);
+
+    // Set CLOCK bit to 0
+    PORTB &= ~CLOCK;
+    _delay_us(6);
+}
+
+#endif
