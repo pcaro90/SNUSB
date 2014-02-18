33602f2d87be92c14406cd7318e7d04b1bf2e2b9
Pablo Caro <me@pcaro.es>
2014-02-18 20:46:52 +0100

    Preparing SNUSB.c

--- src/SNUSB.c
+++ src/SNUSB.c
@@ -18,42 +18,40 @@
 // ---------------------------------------------------
 
 #include <avr/io.h>
-#include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include "usb_keyboard.h"
+#include "gamepad.h"
 
 #define LED_CONFIG	(DDRD |= (1<<6))
 #define LED_ON		(PORTD &= ~(1<<6))
 #define LED_OFF		(PORTD |= (1<<6))
+
 #define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
 
-uint8_t number_keys[10]=
-	{KEY_0,KEY_1,KEY_2,KEY_3,KEY_4,KEY_5,KEY_6,KEY_7,KEY_8,KEY_9};
-
-uint16_t idle_count=0;
+volatile int ready = 1;
 
 int main(void)
 {
-	uint8_t b, d, mask, i, reset_idle;
-	uint8_t b_prev=0xFF, d_prev=0xFF;
-
 	// set for 16 MHz clock
 	CPU_PRESCALE(0);
 
 	// Configure all port B and port D pins as inputs with pullup resistors.
-	// See the "Using I/O Pins" page for details.
-	// http://www.pjrc.com/teensy/pins.html
 	DDRD = 0x00;
 	DDRB = 0x00;
 	PORTB = 0xFF;
 	PORTD = 0xFF;
 
+    // Turn the LED on during the configuration
+    LED_CONFIG;
+    LED_ON;
+
 	// Initialize the USB, and then wait for the host to set configuration.
-	// If the Teensy is powered without a PC connected to the USB port,
-	// this will wait forever.
 	usb_init();
-	while (!usb_configured()) /* wait */ ;
+	while (!usb_configured());
+
+    // Initialize the gamepad interface
+    gamepad_init();
 
 	// Wait an extra second for the PC's operating system to load drivers
 	// and do whatever it does to actually be ready for input
@@ -63,45 +61,20 @@ int main(void)
 	// 256*1024 clock cycles, or approx 61 Hz when using 16 MHz clock
 	// This demonstrates how to use interrupts to implement a simple
 	// inactivity timeout.
-	TCCR0A = 0x00;
-	TCCR0B = 0x05;
+    // Timer 0 configuration (~60Hz)
+	TCCR0A = 0x00;  // Normal mode
+	TCCR0B = 0x05;  // Clock/1024
 	TIMSK0 = (1<<TOIE0);
 
 	while (1) {
-		// read all port B and port D pins
-		b = PINB;
-		d = PIND;
-		// check if any pins are low, but were high previously
-		mask = 1;
-		reset_idle = 0;
-		for (i=0; i<8; i++) {
-			if (((b & mask) == 0) && (b_prev & mask) != 0) {
-				usb_keyboard_press(KEY_B, KEY_SHIFT);
-				usb_keyboard_press(number_keys[i], 0);
-				reset_idle = 1;
-			}
-			if (((d & mask) == 0) && (d_prev & mask) != 0) {
-				usb_keyboard_press(KEY_D, KEY_SHIFT);
-				usb_keyboard_press(number_keys[i], 0);
-				reset_idle = 1;
-			}
-			mask = mask << 1;
-		}
-		// if any keypresses were detected, reset the idle counter
-		if (reset_idle) {
-			// variables shared with interrupt routines must be
-			// accessed carefully so the interrupt routine doesn't
-			// try to use the variable in the middle of our access
-			cli();
-			idle_count = 0;
-			sei();
-		}
-		// now the current pins will be the previous, and
-		// wait a short delay so we're not highly sensitive
-		// to mechanical "bounce".
-		b_prev = b;
-		d_prev = d;
-		_delay_ms(2);
+        while (!ready);  // Block until the next cycle (~60Hz)
+        cli();
+        ready = 0;
+        sei();
+
+        // Read pressed buttons from gamepad interface
+        gamepad_read();
+
 	}
 }
 
@@ -110,11 +83,5 @@ int main(void)
 // will send a space character.
 ISR(TIMER0_OVF_vect)
 {
-	idle_count++;
-	if (idle_count > 61 * 8) {
-		idle_count = 0;
-		usb_keyboard_press(KEY_SPACE, 0);
-	}
+    ready = 1;
 }
-
-
